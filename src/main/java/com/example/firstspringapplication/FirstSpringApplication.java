4bb216360244be7f2b3f7eaac5cf185dafddc0f9
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-27 05:38:58 +0530

    Added: Spring Boot project using IDE

---
+++ src/main/java/com/example/firstspringapplication/FirstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+
+}
